0dbf811c6ae6e8c82622c47cb8db66f22ae3e9cd
AristarhKenebas <Arikkenebas09@gmail.com>
2026-06-15 13:26:07 +0300

    fix: update runtimeConfig for correct API URL in Docker

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   modules: ['@nuxtjs/tailwindcss'],
   runtimeConfig: {
     public: {
-      apiUrl: 'http://localhost:3001'
+      apiUrl: process.env.NUXT_PUBLIC_API_URL || 'http://localhost:3001'
     }
   },
   app: {
